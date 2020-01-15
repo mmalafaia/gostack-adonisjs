33a650a5d6c393a1b79534a3830fe50c4e14617d
Manoel Malafaia <mmalafaia@gmail.com>
2020-01-15 16:34:33 -0300

    Password recovery

--- app/Models/User.js
+++ app/Models/User.js
@@ -1,23 +1,23 @@
-"use strict";
+'use strict'
 
-const Model = use("Model");
+const Model = use('Model')
 
-const Hash = use("Hash");
+const Hash = use('Hash')
 
 class User extends Model {
   static boot() {
-    super.boot();
+    super.boot()
 
-    this.addHook("beforeSave", async userInstance => {
+    this.addHook('beforeSave', async userInstance => {
       if (userInstance.dirty.password) {
-        userInstance.password = await Hash.make(userInstance.password);
+        userInstance.password = await Hash.make(userInstance.password)
       }
-    });
+    })
   }
 
   tokens() {
-    return this.hasMany("App/Models/Token");
+    return this.hasMany('App/Models/Token')
   }
 }
 
-module.exports = User;
+module.exports = User
--- start/routes.js
+++ start/routes.js
@@ -1,5 +1,7 @@
-"use strict";
+'use strict'
 
-const Route = use("Route");
+const Route = use('Route')
 
-Route.post("users", "UserController.store");
+Route.post('users', 'UserController.store')
+Route.post('sessions', 'SessionController.store')
+Route.post('passwords', 'ForgotPasswordController.store')
