fa7f9407bc7c1b9bc76298c2e350c2d312f63667
Manoel Malafaia <mmalafaia@gmail.com>
2020-01-16 16:26:15 -0300

    Relations

--- app/Models/Task.js
+++ app/Models/Task.js
@@ -4,6 +4,17 @@
 const Model = use('Model')
 
 class Task extends Model {
+  project() {
+    return this.belongsTo('App/Models/Projec')
+  }
+
+  user() {
+    return this.belongsTo('App/Models/User')
+  }
+
+  file() {
+    return this.belongsTo('App/Models/File')
+  }
 }
 
 module.exports = Task
