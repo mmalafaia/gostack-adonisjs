589b889528a740a8c5f8fccdc7a7110228920f79
Manoel Malafaia <mmalafaia@gmail.com>
2020-01-21 10:38:36 -0300

    Transactions

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -1,17 +1,27 @@
 'use strict'
 
+const Database = use('Database')
 const User = use('App/Models/User')
 
 class UserController {
   async store({ request }) {
-    const data = request.only(['username', 'email', 'password'])
-    const addresses = request.input('addresses')
+    try {
+      const data = request.only(['username', 'email', 'password'])
+      const addresses = request.input('addresses')
 
-    const user = await User.create(data)
+      const trx = await Database.beginTransaction()
 
-    await user.addresses().createMany(addresses)
+      const user = await User.create(data, trx)
 
-    return user
+      await user.addresses().createMany(addresses, trx)
+
+      await trx.commit()
+
+      return user
+    } catch (error) {
+      await trx.rollback()
+      throw error
+    }
   }
 }
 
